49a7d52c44d82ed76c78ccf425be259c7b47c8f5
Monica Cao <caomon@uw.edu>
2017-01-17 14:00:18 -0800

    Add exercise 5

---
+++ exercise-5.R
@@ -0,0 +1,30 @@
+# Exercise 5: Vector practice
+
+# Create a vector `words` of 6 (or more) words.
+# You can Google for a "random word generator" if you wish!
+words <- c("land","dry","cake","ramen","noodles","fish")
+
+
+# Create a new vector `words.of.the.day` that is your `words` vector with the string
+# "is the word of the day!" pasted on to the end.
+# BONUS: Surround the word in quotes (single or double) (e.g., `"data" is the word of the day!`)
+words.of.the.day <- paste(words, " is the word of the day!")
+
+
+# Create a vector `a.f.words` which are the elements in `words` that start with "a" through "f"
+# Hint: use a comparison operator to see if the word comes before "f" alphabetically!
+# Tip: make sure all the words are lower-case, and only consider the first letter of the word!
+word.a.f <- substring(words, 1, 2) <= 'f'
+a.f.words <- words[word.a.f]
+
+
+# Create a vector `g.m.words' which are the elements in `words` that start with "g" through "m"
+word.g.m <- substring(words, 1, 2) >= 'g' & substring(words, 1, 2) <= 'm'
+g.m.words <- words[word.g.m]
+
+
+# Define a function `WordBin` that takes in three arguments: a vector of words, and two letters. The function
+# should return a vector of words that go between those letters alphabetically.
+
+
+# Use your `WordBin` function to determine which of your words start with "e" through "q"
